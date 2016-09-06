f99bec7a98a73b7ebcf711e2c0e51f0e436d3a13
yclzone <yclzone@gmail.com>
2016-09-06 14:12:15 +0800

    Initial Commit

---
+++ QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by YCLZONE on 9/6/16.
+//  Copyright © 2016 YCLZONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRCode
+//
+//  Created by YCLZONE on 9/6/16.
+//  Copyright © 2016 YCLZONE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
